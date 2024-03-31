5fdad9795f82625bb47b3a4eb32b8b08148136af
niravpatel6656 <npatel8705@conestogac.on.ca>
2024-03-30 21:40:17 -0400

    write a code to open a file from different location and change the name and save files to the different location.

---
+++ Mail Merge Project Start/main.py
@@ -0,0 +1,20 @@
+#TODO: Create a letter using starting_letter.txt 
+#for each name in invited_names.txt
+#Replace the [name] placeholder with the actual name.
+#Save the letters in the folder "ReadyToSend".
+    
+#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open("./Input/Letters/starting_letter.txt") as file:
+    content = file.read()
+
+with open("./Input/Names/invited_names.txt") as file:
+    name_list = file.readlines()
+
+for j in name_list:
+    file_path = f"./Output/ReadyToSend/invite_for_{j}".strip("\n") + ".txt"
+    with open(file_path, mode="w") as file:
+        new_content = content.replace("[name]", j)
+        file.write(new_content)
